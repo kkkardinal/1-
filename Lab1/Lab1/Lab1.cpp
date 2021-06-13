bbc84f8d4611443f7a8421d3b15e5d883eb140c1
kkkardinal <76873845+kkkardinal@users.noreply.github.com>
2021-06-14 00:48:24 +0300

    Add files via upload

---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	int const size = 1000000;
+	int temp=0;
+	int* array = new int[size];
+	ofstream fout1("input.txt");
+	for (int i = 0; i < size; i++) {
+		array[i] = rand()%5000;
+		fout1 << array[i] << "\t";
+	}
+	fout1.close();
+
+	for (int i = 0; i < size; i++) {
+
+		for (int j = 0; j < size-1; j++) {
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+	
+	
+	ofstream fout("output.txt");
+	for (int i = 0; i < size; i++) {
+		fout << array[i] << "\t";
+	}
+	fout.close();
+	return 0;
+}
+
